c6adabce098fcadce9c7e72d5e658f0cfa94e3bd
Mason <moontough8@gmail.com>
2025-01-12 16:50:24 +0900

    [level 1] Title: 카드 뭉치, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,15 @@
+function solution(cards1, cards2, goal) {
+    var answer = "Yes";
+    
+    for(let i=0; i<goal.length; i++){
+        if (cards1.length > 0 && goal[i] === cards1[0]) {
+            cards1.shift();
+        } else if (cards2.length > 0 && goal[i] === cards2[0]) {
+            cards2.shift(); 
+        } else {
+            answer = "No"; 
+            break;
+        }
+    } 
+    return answer;
+}
