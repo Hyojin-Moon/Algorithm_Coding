64a32b7ac64444533042b7683e4c8dff6c70a002
Mason <moontough8@gmail.com>
2025-01-21 08:54:42 +0900

    [level 1] Title: 소수 만들기, Time: 122.13 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,17 @@
+function solution(nums) {
+    let answer = 0;
+    let sum = 0;
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i + 1; j < nums.length; j++) {
+            for(let k = j + 1; k < nums.length; k++) {
+                sum = nums[i] + nums[j] + nums[k];
+                let count = 0;
+                for(let n = 1; n <= sum; n++) {
+                    if(sum % n === 0) count++;
+                }
+                if (count === 2) answer++;
+            }
+        }
+    }
+    return answer;
+}
